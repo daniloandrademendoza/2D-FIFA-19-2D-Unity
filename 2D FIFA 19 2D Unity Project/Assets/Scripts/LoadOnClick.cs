0bc916c17b53d8d522820dc83366c26b0ce07af8
Danilo Andrade Mendoza <daniloandradem10@email.arizona.edu>
2018-10-02 23:44:51 -0700

    Added the User Interface to my game.

---
+++ 2D FIFA 19 2D Unity Project/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+//using System.Collections.Generic;
+
+public class LoadOnClick : MonoBehaviour {
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene("SampleScene");
+       
+    }
+
+}
